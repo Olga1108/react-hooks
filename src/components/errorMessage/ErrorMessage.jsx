fdab255d6afe22a0a812487dea1be6db4e859f6c
Olha Pushkar <Olha_Pushkar@epam.com>
2021-11-22 14:12:49 +0300

    add:all files before FSM

---
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -0,0 +1,8 @@
+import img from './error2.gif';
+const ErrorMessage = () => {
+    return (
+        <img style={{display: 'block', height: '260px', margin: '0 auto'}} src={img} alt="error"/>
+    )
+}
+
+export default ErrorMessage;
